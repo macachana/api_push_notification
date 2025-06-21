a8c2fefa0e5e7fbd59a5a839837a6ea92293280c
macarena <macarena.lani@gmail.com>
2025-06-21 02:37:32 -0300

    m

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const serviceAccount = require(process.env.FIREBASE_CONFIG);
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
